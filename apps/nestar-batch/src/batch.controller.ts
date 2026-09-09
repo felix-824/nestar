a63d7512d71f09e5fb52ff2ba82bf91a1ce480ea
felix-824 <nf94new@gmail.com>
2026-09-09 23:37:52 +0900

    feat: create: batchProperties and batchAgents jobSchedulers logic A

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,61 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_PROPERTIES, BATCH_TOPAGENST } from './lib/config';
 
 @Controller()
 export class BatchController {
-	constructor(private readonly nestarBatchService: BatchService) {}
+  private logger: Logger = new Logger('BatchController');
+
+	constructor(private readonly BatchService: BatchService) {}
+
+  @Timeout(1000)
+  handleTimeout(){
+    this.logger.debug('BATCH SERVER READY!');
+  }
+
+  @Cron("00 * * * * *",  {name: BATCH_ROLLBACK})
+  public async batchRollback() {
+    try{
+       this.logger['context'] = BATCH_ROLLBACK; 
+       this.logger.debug('EXECUTED!');
+       await this.BatchService.batchRollback();
+    } catch(err) {
+      this.logger.error(err);
+    }
+  }
+
+   @Cron("20 * * * * *",  {name: BATCH_TOP_PROPERTIES})
+  public async batchProperties() {
+    try{
+       this.logger['context'] = BATCH_TOP_PROPERTIES; 
+       this.logger.debug('EXECUTED!');
+       await this.BatchService.batchProperties();
+    } catch(err) {
+      this.logger.error(err);
+    }
+  }
+
+   @Cron("40 * * * * *",  {name: BATCH_TOPAGENST})
+  public async batchAgents() {
+    try{
+     this.logger['context'] = BATCH_TOPAGENST; 
+     this.logger.debug('EXECUTED!');
+     await this.BatchService.batchAgents();
+    } catch(err) {
+      this.logger.error(err);
+    }
+  }
+
+  /** 
+   @Interval(1000)
+  handleInterval(){
+    this.logger.debug('Interval TEST');
+  }
+  **/
 
 	@Get()
 	getHello(): string {
-		return this.nestarBatchService.getHello();
+		return this.BatchService.getHello();
 	}
 }
---
+++ apps/nestar-batch/src/lib/config.ts
@@ -0,0 +1,7 @@
+/***************************
+ *       BATCH CONSTANS  *
+ **************************/
+
+export const BATCH_ROLLBACK = 'BATCH_ROLLBACK';
+export const BATCH_TOP_PROPERTIES = 'BATCH_TOP_PROPERTIES';
+export const BATCH_TOPAGENST = 'BATCH_TOPAGENST';
